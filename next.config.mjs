51451845fc41c158e6ff74f268b9ba17860be363
Igor Barcik <igor1606553@gmail.com>
2024-04-28 23:02:31 +0200

    Docker compose works!

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  logging: {
-    fetches: {
-      fullUrl: true,
-    },
-  },
+  // logging: {
+  //   fetches: {
+  //     fullUrl: true,
+  //   },
+  // },
+  output: "standalone",
 };
 
 export default nextConfig;
